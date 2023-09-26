c07c6803cae32807730ef3c5df5e4f98850d58b7
Tasadbee <ttasadbee@gmail.com>
2023-09-27 01:38:37 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var a = prompt ('Yoshingizni kiriting:')
+if(a <= 17){
+console.log('Welcome to our site little brother');
+}else if (a >= 19){
+console.log('Welcome to our site Big brother');}
+else {
+    console.log('Welcome to our site my friend');
+}
